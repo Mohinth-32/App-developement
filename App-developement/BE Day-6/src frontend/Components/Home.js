36c88ed6b4c8226517da18f2d4a291378a4d49b4
Mohinth <110531945+Mohinth-32@users.noreply.github.com>
2023-10-12 15:00:00 +0530

    Add files via upload

---
+++ App-developement/BE Day-6/src frontend/Components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <div className='background-container'>
+      <div className='forall'>
+      welcome
+      </div>
+      
+      
+    </div>
+  );
+}
+
+export default Home;
