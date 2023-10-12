4fa24417a9b5e6ed7dbbd773072af4d3aee349fd
Mohinth <110531945+Mohinth-32@users.noreply.github.com>
2023-10-12 15:05:07 +0530

    Add files via upload

---
+++ App-developement/BE Day-8/src frontend/Components/ExpenseForm.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+
+function ExpenseForm({ onExpenseAdd }) {
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onExpenseAdd({ description, amount: parseFloat(amount) });
+    setDescription('');
+    setAmount('');
+  };
+  
+
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Expense Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Expense Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button type="submit">Add Expense</button>
+    </form>
+    
+    </>
+  );
+}
+
+export default ExpenseForm;
---
+++ App-developement/BE Day-8/src frontend/Components/FooterStyles.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+padding: 5px 5px;
+background: black;
+position: right;
+padding-left:20%;
+padding-bottom: 1px;
+width: 100%;
+
+
+@media (max-width: 1000px) {
+	padding: 70px 30px;
+}
+`;
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: ce;
+	max-width: 1000px;
+	margin: -11px;
+	/* background: red; */
+`
+
+export const Column = styled.div`
+display: flex;
+flex-direction: column;
+text-align: right;
+margin-lift: 60px;
+`;
+
+export const Row = styled.div`
+display: grid;
+grid-template-columns: repeat(auto-fill,
+						minmax(185px, 1fr));
+grid-gap: 20px;
+
+@media (max-width: 1000px) {
+	grid-template-columns: repeat(auto-fill,
+						minmax(200px, 1fr));
+}
+`;
+
+export const FooterLink = styled.a`
+color: #fff;
+margin-bottom: 20px;
+font-size: 18px;
+text-decoration: none;
+
+&:hover {
+	color: green;
+	transition: 200ms ease-in;
+}
+`;
+
+export const Heading = styled.p`
+font-size: 24px;
+color: #fff;
+margin-bottom: 40px;
+font-weight: bold;
+`;
